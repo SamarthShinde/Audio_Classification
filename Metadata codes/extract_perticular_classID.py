78e9a39614d5d95cb9efee85537214af3909ba98
SamarthShinde <samarth.shinde505@gmail.com>
2024-08-29 11:38:41 +0530

    made change to code to extract desired class from the csv & convert into mp3 & stereo audio.

--- Metadata codes/extract_perticular_classID.py
+++ Metadata codes/extract_perticular_classID.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import shutil
 import os
+from pydub import AudioSegment
 
 # Define the path to the CSV file and the base directory containing the audio files
-csv_path = '/Volumes/DATA_on_server/AUDIO_DATA/UrbanSound8K/metadata/UrbanSound8K.csv'
-audio_base_dir = '/Volumes/DATA_on_server/AUDIO_DATA/UrbanSound8K/audio'
-target_dir = '/Users/samarthshinde/Desktop/ready_codes/Audio_Classification_ML/music'
+csv_path = '/Users/samarthshinde/Desktop/Backup/ready_codes/Audio_Classification_ML/to_record/metadata_test.csv'
+audio_base_dir = '/Volumes/T7_Shield/Audio/to_record/audiorec'
+target_dir = '/Users/samarthshinde/Desktop/Male_Female'
 
 # Create the target directory if it does not exist
 os.makedirs(target_dir, exist_ok=True)
@@ -13,17 +14,32 @@ os.makedirs(target_dir, exist_ok=True)
 # Read the CSV file
 metadata = pd.read_csv(csv_path)
 
-# Filter the metadata for classID 9
-class_9_metadata = metadata[metadata['classID'] == 9]
+# Filter the metadata for classID 1 (assuming 1 corresponds to Male and Female classes)
+class_9_metadata = metadata[metadata['classID'] == 2]
 
-# Copy the audio files to the target directory
+
+# Function to check if the audio is mono and convert it to stereo if necessary
+def convert_to_stereo(audio):
+    if audio.channels == 1:
+        return audio.set_channels(2)
+    return audio
+
+
+# Process and copy the audio files to the target directory
 for index, row in class_9_metadata.iterrows():
     fold = row['fold']
     file_name = row['slice_file_name']
     source_path = os.path.join(audio_base_dir, f'fold{fold}', file_name)
-    target_path = os.path.join(target_dir, file_name)
+    target_mp3_path = os.path.join(target_dir, f"{os.path.splitext(file_name)[0]}.mp3")
 
-    # Copy the file
-    shutil.copy(source_path, target_path)
+    # Load the audio file and convert it to MP3
+    if os.path.exists(source_path):
+        audio = AudioSegment.from_wav(source_path)
 
-print(f"Copied {len(class_9_metadata)} files to {target_dir}")
+        # Convert to stereo if the audio is mono
+        audio_stereo = convert_to_stereo(audio)
+
+        # Export the audio to MP3 format
+        audio_stereo.export(target_mp3_path, format="mp3")
+
+print(f"Copied and converted {len(class_9_metadata)} files to {target_dir}")
